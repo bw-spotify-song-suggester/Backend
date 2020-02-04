01e5503eed7404ff348474a815ce16019c576cfe
Allie Robinson <allierob93@gmail.com>
2020-02-04 11:26:32 -0600

    added helmet

--- api/server.js
+++ api/server.js
@@ -3,7 +3,9 @@ const authRouter = require('../auth/auth-router');
 const userRouter = require('../users/users-router');
 const server = express();
 const cors = require('cors');
+const helmet = require('helmet');
 
+server.use(helmet());
 server.use(express.json());
 server.use('/api/auth', authRouter);
 server.use('/api/user', userRouter);
