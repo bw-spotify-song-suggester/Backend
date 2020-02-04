a13c4eed59a255adc411e0b7505864854619048e
Allie Robinson <allierob93@gmail.com>
2020-02-04 11:24:37 -0600

    bug fixing

--- api/server.js
+++ api/server.js
@@ -2,19 +2,17 @@ const express = require('express');
 const authRouter = require('../auth/auth-router');
 const userRouter = require('../users/users-router');
 const server = express();
-var cors = require('cors');
+const cors = require('cors');
 
+server.use(express.json());
+server.use('/api/auth', authRouter);
+server.use('/api/user', userRouter);
 server.use(
     cors({
         credentials: true,
         origin: true
     })
 );
-
 server.options('*', cors());
-
-server.use(express.json());
-server.use('/api/auth', authRouter);
-server.use('/api/user', userRouter);
 
 module.exports = server;
